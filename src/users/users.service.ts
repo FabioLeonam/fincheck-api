5cedf875d2937cef78b8d57cf13bf438e1dbd2ba
Fabio Leonam <f.leonam@hotmail.com>
2023-06-23 20:01:50 -0300

    Adding encrypt in password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
-
+import { hash } from 'bcryptjs';
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-    const user = await this.prismaService.user.create({
-      data: {
-        name: createUserDto.name,
-        email: createUserDto.email,
-        password: createUserDto.password,
-      },
+    const { name, email, password } = createUserDto;
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: { email },
+      select: { id: true },
     });
-    return user;
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use');
+    }
+
+    const hashedPassword = await hash(password, 12);
+    const user = await this.prismaService.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+    return {
+      name: user.name,
+      email: user.email,
+    };
   }
 }
