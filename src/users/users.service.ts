5ca7900b6e3068898707416eea575d4c79e00a6b
Fabio Leonam <f.leonam@hotmail.com>
2023-06-22 21:39:11 -0300

    Creating post route to create a new user, validating data using pipe and connecting to db

---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prismaService.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: createUserDto.password,
+      },
+    });
+    return user;
+  }
+}
