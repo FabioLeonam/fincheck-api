ccc6bdbdb94f84d9577febed33a2f638013911e1
Fabio Leonam <f.leonam@hotmail.com>
2023-06-26 20:47:28 -0300

    Creating auth module, adding configs to .env file

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthenticateDto } from './dto/authenticate.dto';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userRepository: UsersRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async authenticate(authenticateDto: AuthenticateDto) {
+    const { email, password } = authenticateDto;
+    const user = await this.userRepository.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken = await this.jwtService.signAsync({ sub: user.id });
+
+    return { accessToken };
+  }
+}
