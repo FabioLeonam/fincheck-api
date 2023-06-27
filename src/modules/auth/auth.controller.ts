ae0b655a0a1e0e4768dbcf1f00d8a144ed08504b
Fabio Leonam <f.leonam@hotmail.com>
2023-06-26 21:21:17 -0300

    Moving createUser to authModule and rename it to signUp

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { SignInDto } from './dto/signin.dto';
+import { SignUpDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  authenticate(@Body() authenticateDto: AuthenticateDto) {
-    return this.authService.authenticate(authenticateDto);
+  signin(@Body() signInDto: SignInDto) {
+    return this.authService.signin(signInDto);
+  }
+
+  @Post('signup')
+  create(@Body() signUpDto: SignUpDto) {
+    return this.authService.signup(signUpDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,19 +1,24 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { SignInDto } from './dto/signin.dto';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userRepository: UsersRepository,
+    private readonly usersRepository: UsersRepository,
     private readonly jwtService: JwtService,
   ) {}
 
-  async authenticate(authenticateDto: AuthenticateDto) {
-    const { email, password } = authenticateDto;
-    const user = await this.userRepository.findUnique({
+  async signin(signInDto: SignInDto) {
+    const { email, password } = signInDto;
+    const user = await this.usersRepository.findUnique({
       where: { email },
     });
 
@@ -27,8 +32,57 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    const accessToken = await this.jwtService.signAsync({ sub: user.id });
+    const accessToken = await this.generateAccessToken(user.id);
 
     return { accessToken };
+  }
+
+  async signup(signupDto: SignUpDto) {
+    const { name, email, password } = signupDto;
+    const emailTaken = await this.usersRepository.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use');
+    }
+
+    const hashedPassword = await hash(password, 12);
+    const user = await this.usersRepository.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              //Income
+              { name: 'Salary', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Other', icon: 'other', type: 'INCOME' },
+              //Expense
+              { name: 'Home', icon: 'home', type: 'EXPENSE' },
+              { name: 'Food', icon: 'food', type: 'EXPENSE' },
+              { name: 'Education', icon: 'education', type: 'EXPENSE' },
+              { name: 'Leisure', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Grocery', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Clothes', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transport', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Trips', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
+    });
+
+    const accessToken = await this.generateAccessToken(user.id);
+
+    return { accessToken };
+  }
+
+  private generateAccessToken(userId: string) {
+    return this.jwtService.signAsync({ sub: userId });
   }
 }
