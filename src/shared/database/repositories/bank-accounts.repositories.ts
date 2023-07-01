916b8f48068bdc79a062d229e30e46293b1eccb0
Fabio Leonam <f.leonam@hotmail.com>
2023-07-01 16:01:13 -0300

    Creating bankAccounts repository

---
+++ src/shared/database/repositories/bank-accounts.repositories.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class BankAccountsRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createDto: Prisma.BankAccountCreateArgs) {
+    return this.prismaService.bankAccount.create(createDto);
+  }
+
+  findMany(findManyDto: Prisma.BankAccountFindManyArgs) {
+    return this.prismaService.bankAccount.findMany(findManyDto);
+  }
+}
