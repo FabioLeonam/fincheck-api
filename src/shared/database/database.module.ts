cdaca3defbd902cbeea4356b290ee4ee46844d2d
Fabio Leonam <f.leonam@hotmail.com>
2023-07-01 16:00:51 -0300

    Creating bankAccounts module

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -2,10 +2,16 @@ import { Global, Module } from '@nestjs/common';
 import { UsersRepository } from './repositories/users.repositories';
 import { PrismaService } from './prisma.service';
 import { CategoriesRepository } from './repositories/categories.repositories';
+import { BankAccountsRepository } from './repositories/bank-accounts.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository, CategoriesRepository],
-  exports: [UsersRepository, CategoriesRepository],
+  providers: [
+    PrismaService,
+    UsersRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+  ],
+  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository],
 })
 export class DatabaseModule {}
