45ca3455610f457b0978ac65ee65a5c31fb161e1
Fabio Leonam <f.leonam@hotmail.com>
2023-07-01 15:00:42 -0300

    Creating categories module

---
+++ src/modules/categories/categories.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { CategoriesRepository } from 'src/shared/database/repositories/categories.repositories';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly categoriesRepository: CategoriesRepository) {}
+
+  findAllByUserId(userId: string) {
+    return this.categoriesRepository.findMany({
+      where: { userId },
+      select: {
+        id: true,
+        name: true,
+        icon: true,
+      },
+    });
+  }
+}
