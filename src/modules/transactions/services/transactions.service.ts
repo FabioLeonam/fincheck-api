085bd57888b9dafc19dd82ca223a802c9a6dbf31
Fabio Leonam <f.leonam@hotmail.com>
2023-07-04 20:30:03 -0300

    Adding month and year as filters in findAll route from transaction service

--- src/modules/transactions/services/transactions.service.ts
+++ src/modules/transactions/services/transactions.service.ts
@@ -37,9 +37,15 @@ export class TransactionsService {
     });
   }
 
-  findAllByUserId(userId: string) {
+  findAllByUserId(userId: string, filters: { month: number; year: number }) {
     return this.transactionsRepository.findMany({
-      where: { userId },
+      where: {
+        userId,
+        date: {
+          gte: new Date(Date.UTC(filters.year, filters.month)),
+          lt: new Date(Date.UTC(filters.year, filters.month + 1)),
+        },
+      },
     });
   }
 
